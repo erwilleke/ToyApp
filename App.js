7c6ae0d9e12805ea4224a2cfa4918d0f080864f4
Eric Willeke <eric.willeke@gmail.com>
2012-10-25 20:43:30 -0400

    Displays Hello World!

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-
+	items: [
+		{
+			html: 'Does this work?'
+		
+		}
+	],
     launch: function() {
         //Write app code here
+		console.log("Hello, you silly dev!");
+		console.log( this );
     }
 });
