50ac140f884515bd27edd65a066077bdf2d263fd
Eric Willeke <eric.willeke@gmail.com>
2012-10-25 21:05:25 -0400

    Dynamically adds a child to a panel

--- App.js
+++ App.js
@@ -2,21 +2,27 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 	layout: {
-		type: 'accordion'
+		type: 'hbox',
+		pack: 'start',
+		align: 'stretch'
 	},
 	items: [
 		{
             xtype: 'panel',
             title: 'Left of the world',
+			width: 250,
 			html: 'Hello, world left'
         },
         {
             xtype: 'panel',
+			itemId: 'rightSide',
+			flex: 1,
             title: 'Right of the world',
 			html: 'Hello, world right 1/4'
 		},
         {
             xtype: 'panel',
+			flex: 3,
             title: 'Righter of the world',
 			html: 'Hello, world righter 3/4'
 		}
@@ -24,6 +30,7 @@ Ext.define('CustomApp', {
     launch: function() {
         //Write app code here
 		console.log("Hello, you silly dev!");
-		console.log( this );
+
+		this.down('#rightSide').add( {xtype: 'panel', title: 'Whee!'} );
     }
 });
