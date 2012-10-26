60185ac1730159c2f5ce66470d4ccf53010e3ce9
Eric Willeke <eric.willeke@gmail.com>
2012-10-25 21:47:47 -0400

    Displays fields from a single story
    Update it with a story you own or can see (using story detail URL).
    This page is useful to look at when understanding this one
    http://developer.rallydev.com/apps/2.0p4/doc/#!/guide/appsdk_20_data_models

--- App.js
+++ App.js
@@ -7,30 +7,60 @@ Ext.define('CustomApp', {
 		align: 'stretch'
 	},
 	items: [
-		{
-            xtype: 'panel',
-            title: 'Left of the world',
-			width: 250,
-			html: 'Hello, world left'
-        },
         {
             xtype: 'panel',
 			itemId: 'rightSide',
 			flex: 1,
-            title: 'Right of the world',
-			html: 'Hello, world right 1/4'
+            title: 'Right of the world'
 		},
         {
             xtype: 'panel',
+			itemId: 'furtherRight',
 			flex: 3,
-            title: 'Righter of the world',
-			html: 'Hello, world righter 3/4'
+            title: 'Righter of the world'
 		}
 	],
     launch: function() {
         //Write app code here
 		console.log("Hello, you silly dev!");
 
-		this.down('#rightSide').add( {xtype: 'panel', title: 'Whee!'} );
-    }
+		
+		Rally.data.ModelFactory.getModel({
+			type: 'UserStory',
+			scope: this, // hint: this brings callback into the 'this' context
+			success: function( model ){
+				console.log( "Back from model creation!" );
+				this.useModel( model );
+				}
+			});
+			
+		console.log("This should happen before call returns!");
+    },
+	useModel: function( model ) {
+		model.load( '9000433564', // This is the number at the end of the user story detail page URL
+			{ 
+				fetch: ['Name', 'ScheduleState', 'Owner' ],
+				scope: this, // hint: Don't forget this!
+				callback: function( result, operation ) {
+					if( operation.wasSuccessful() ) {
+						this.down('#furtherRight').add( {
+							xtype: 'panel', 
+							title: 'Woot!',
+							items: [
+								{ html: result.data.Owner._refObjectName},
+								{ html: result.data.ScheduleState},
+								{ html: result.data.Name},
+								],
+							layout: { type: 'vbox'}
+							});
+					console.log( result );
+					}
+					else {
+						this.down( '#furtherRight' ).update( 'Doh!' );
+					}
+				}				
+			} 
+		);	
+	}
+	
 });
