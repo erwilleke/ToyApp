9b36bd8e678191169193f503336ac5cfda80b9b5
Eric Willeke <eric.willeke@gmail.com>
2012-10-25 21:00:35 -0400

    Now has a basic accordion layout for good measure

--- App.js
+++ App.js
@@ -2,26 +2,21 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 	layout: {
-		type: 'hbox',
-		pack: 'start',
-		align: 'stretch'
+		type: 'accordion'
 	},
 	items: [
 		{
             xtype: 'panel',
             title: 'Left of the world',
-			width: 250,
 			html: 'Hello, world left'
         },
         {
             xtype: 'panel',
-			flex: 1,
             title: 'Right of the world',
 			html: 'Hello, world right 1/4'
 		},
         {
             xtype: 'panel',
-			flex: 3,
             title: 'Righter of the world',
 			html: 'Hello, world righter 3/4'
 		}
